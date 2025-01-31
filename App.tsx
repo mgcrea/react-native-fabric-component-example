1d81dc71d974fd82d5ba1a1b5b353544f42d37c4
Olivier Louvignes <olivier@mgcrea.io>
2025-01-31 23:52:16 +0100

    feat(component): add NativeColorView

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Alert, StyleSheet, View} from 'react-native';
-import WebView from './specs/WebViewNativeComponent';
+import {StyleSheet, View} from 'react-native';
+import NativeColorView from './specs/ColorViewNativeComponent';
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <WebView
+      {/* <WebView
         sourceURL="https://react.dev/"
         style={styles.webview}
         onScriptLoaded={() => {
           Alert.alert('Page Loaded');
         }}
-      />
+      /> */}
+      <NativeColorView color="#32a852" style={styles.box} />
     </View>
   );
 }
@@ -19,12 +20,19 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
-    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'palegreen',
   },
   webview: {
     width: '100%',
     height: '100%',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
 });
 
